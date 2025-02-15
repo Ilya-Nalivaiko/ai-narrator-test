d00032807bf6093a6f1737ab80246050a69567b0
Cat6172 <65912861+Ilya-Nalivaiko@users.noreply.github.com>
2025-02-15 12:47:54 -0700

    works but buggy needs cd

--- src/client/java/bbw/narratortest/EventCalls.java
+++ src/client/java/bbw/narratortest/EventCalls.java
@@ -2,42 +2,71 @@ package bbw.narratortest;
 
 import net.minecraft.client.MinecraftClient;
 import net.minecraft.client.network.ClientPlayerEntity;
+import net.minecraft.entity.Entity;
+import net.minecraft.entity.LivingEntity;
+import net.minecraft.entity.damage.DamageSource;
+import net.minecraft.entity.player.PlayerEntity;
 import net.minecraft.item.ItemStack;
 import net.minecraft.screen.CraftingScreenHandler;
-import net.minecraft.screen.slot.SlotActionType;
 import net.minecraft.text.Text;
+import net.minecraft.util.ActionResult;
+import net.minecraft.util.Hand;
+import net.minecraft.util.hit.EntityHitResult;
+import net.minecraft.world.World;
+import org.jetbrains.annotations.Nullable;
 
 public class EventCalls {
     private static ItemStack lastCraftedItem = ItemStack.EMPTY;
     private static boolean itemCraftedFlag = false;
 
+    // Called on client tick
     public static void onClientTick(MinecraftClient client) {
         ClientPlayerEntity player = MinecraftClient.getInstance().player;
-        if (player != null && player.currentScreenHandler instanceof CraftingScreenHandler) {
+        if (player != null) {
+            itemCrafted(player);
+        }
+    }
+
+    // Handles crafting events
+    private static void itemCrafted(ClientPlayerEntity player) {
+        if (player.currentScreenHandler instanceof CraftingScreenHandler) {
             CraftingScreenHandler craftingScreenHandler = (CraftingScreenHandler) player.currentScreenHandler;
             ItemStack outputSlotStack = craftingScreenHandler.getSlot(0).getStack();
 
-            if (!outputSlotStack.isEmpty() && !ItemStack.areEqual(outputSlotStack, lastCraftedItem) && !isAir(outputSlotStack)) {
+            if (!outputSlotStack.isEmpty() && !ItemStack.areEqual(outputSlotStack, lastCraftedItem)) {
                 lastCraftedItem = outputSlotStack.copy();
                 itemCraftedFlag = true;
             } else if (outputSlotStack.isEmpty() && itemCraftedFlag) {
-                itemCrafted(player);
+                for (ItemStack stack : player.getInventory().main) {
+                    if (ItemStack.areEqual(stack, lastCraftedItem)) {
+                        player.sendMessage(Text.literal("You just crafted: " + lastCraftedItem.getName().getString()), false);
+                        break;
+                    }
+                }
                 lastCraftedItem = ItemStack.EMPTY; // Reset the last crafted item
                 itemCraftedFlag = false;
             }
         }
     }
 
-    private static void itemCrafted(ClientPlayerEntity player) {
-        for (ItemStack stack : player.getInventory().main) {
-            if (ItemStack.areEqual(stack, lastCraftedItem)) {
-                player.sendMessage(Text.literal("You just crafted: " + lastCraftedItem.getName().getString()), false);
-                break;
-            }
+    // Handles finishing using an item (e.g., eating food)
+    public static void onFinishUsingItem(ItemStack stack, World world, LivingEntity user) {
+        if (user instanceof PlayerEntity) {
+            PlayerEntity player = (PlayerEntity) user;
+            player.sendMessage(Text.literal("You just ate: " + stack.getName().getString()), false);
         }
     }
 
-    private static boolean isAir(ItemStack stack) {
-        return stack.isEmpty();
+    // Handles attacking entities
+    public static ActionResult onEntityDamage(PlayerEntity player, World world, Hand hand, Entity entity, @Nullable EntityHitResult hitResult) {
+        if (entity instanceof LivingEntity) {
+            player.sendMessage(Text.literal("You just hit: " + entity.getName().getString()), false);
+        }
+        return ActionResult.PASS;
+    }
+
+    // Handles killing entities
+    public static void onEntityKill(PlayerEntity player, World world, LivingEntity killedEntity, @Nullable DamageSource source) {
+        player.sendMessage(Text.literal("You just killed: " + killedEntity.getName().getString()), false);
     }
 }
--- src/client/java/bbw/narratortest/NarratorTestClient.java
+++ src/client/java/bbw/narratortest/NarratorTestClient.java
@@ -2,10 +2,32 @@ package bbw.narratortest;
 
 import net.fabricmc.api.ClientModInitializer;
 import net.fabricmc.fabric.api.client.event.lifecycle.v1.ClientTickEvents;
+import net.fabricmc.fabric.api.event.player.AttackEntityCallback;
+import net.fabricmc.fabric.api.event.player.UseItemCallback;
+import net.minecraft.util.ActionResult;
+import net.minecraft.item.ItemStack;
+import net.minecraft.entity.LivingEntity;
+import net.minecraft.world.World;
 
 public class NarratorTestClient implements ClientModInitializer {
     @Override
     public void onInitializeClient() {
+        // Register client tick event
         ClientTickEvents.END_CLIENT_TICK.register(EventCalls::onClientTick);
+
+        // Register attack entity event
+        AttackEntityCallback.EVENT.register((player, world, hand, entity, hitResult) -> {
+            ActionResult result = EventCalls.onEntityDamage(player, world, hand, entity, hitResult);
+            return result;
+        });
+
+        // Register finish using item event
+        UseItemCallback.EVENT.register((player, world, hand) -> {
+            ItemStack stack = player.getStackInHand(hand);
+            if (!world.isClient) { //TODO:Check if the item is food
+                EventCalls.onFinishUsingItem(stack, world, player);
+            }
+            return ActionResult.PASS; // Return PASS to allow the item usage to proceed
+        });
     }
 }
