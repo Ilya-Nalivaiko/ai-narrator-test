c39d123eabe6cce13c57e61a517a85bd10b5a4d1
Cat6172 <65912861+Ilya-Nalivaiko@users.noreply.github.com>
2025-02-15 17:47:01 -0700

    use block

--- src/client/java/bbw/narratortest/NarratorTestClient.java
+++ src/client/java/bbw/narratortest/NarratorTestClient.java
@@ -5,11 +5,50 @@ import net.fabricmc.fabric.api.client.command.v2.ClientCommandRegistrationCallba
 import net.fabricmc.fabric.api.client.event.lifecycle.v1.ClientTickEvents;
 import net.fabricmc.fabric.api.event.player.AttackEntityCallback;
 import net.fabricmc.fabric.api.event.player.UseBlockCallback;
-import net.minecraft.util.ActionResult;
-import net.minecraft.util.math.BlockPos;
+import net.minecraft.block.AbstractBannerBlock;
+import net.minecraft.block.AbstractSignBlock;
+import net.minecraft.block.AnvilBlock;
+import net.minecraft.block.BarrelBlock;
+import net.minecraft.block.BeaconBlock;
+import net.minecraft.block.BellBlock;
+import net.minecraft.block.Block;
+import net.minecraft.block.BlockState;
+import net.minecraft.block.BrewingStandBlock;
+import net.minecraft.block.ButtonBlock;
+import net.minecraft.block.CampfireBlock;
+import net.minecraft.block.CartographyTableBlock;
+import net.minecraft.block.ChestBlock;
+import net.minecraft.block.CommandBlock;
+import net.minecraft.block.ComparatorBlock;
+import net.minecraft.block.CraftingTableBlock;
+import net.minecraft.block.DispenserBlock;
+import net.minecraft.block.DoorBlock;
+import net.minecraft.block.DropperBlock;
+import net.minecraft.block.EnchantingTableBlock;
+import net.minecraft.block.EnderChestBlock;
+import net.minecraft.block.FenceGateBlock;
+import net.minecraft.block.FurnaceBlock;
+import net.minecraft.block.GrindstoneBlock;
+import net.minecraft.block.HopperBlock;
+import net.minecraft.block.JigsawBlock;
+import net.minecraft.block.JukeboxBlock;
+import net.minecraft.block.LecternBlock;
+import net.minecraft.block.LeverBlock;
+import net.minecraft.block.LoomBlock;
+import net.minecraft.block.NoteBlock;
+import net.minecraft.block.RepeaterBlock;
+import net.minecraft.block.RespawnAnchorBlock;
+import net.minecraft.block.ShulkerBoxBlock;
+import net.minecraft.block.SmithingTableBlock;
+import net.minecraft.block.StonecutterBlock;
+import net.minecraft.block.StructureBlock;
+import net.minecraft.block.TrapdoorBlock;
+import net.minecraft.client.network.ClientPlayerEntity;
 import net.minecraft.item.BlockItem;
 import net.minecraft.item.ItemStack;
 import net.minecraft.text.Text;
+import net.minecraft.util.ActionResult;
+import net.minecraft.util.math.BlockPos;
 
 public class NarratorTestClient implements ClientModInitializer {
     @Override
@@ -34,20 +73,73 @@ public class NarratorTestClient implements ClientModInitializer {
 
         // Register block interaction event (client-side)
         UseBlockCallback.EVENT.register((player, world, hand, hitResult) -> {
-            if (world.isClient && player != null) {
-                // Log the block interaction
-                player.sendMessage(Text.literal("[DEBUG] You interacted with a block at " + hitResult.getBlockPos().toShortString()), false);
-                NarratorTest.eventLogger.appendEvent("Interact Block", hitResult.getBlockPos().toShortString(), System.currentTimeMillis());
+            if (world.isClient && player instanceof ClientPlayerEntity) {
+                BlockPos interactedPos = hitResult.getBlockPos();
+                BlockState interactedState = world.getBlockState(interactedPos);
+                Block interactedBlock = interactedState.getBlock();
 
-                // Check if the player is placing a block
-                ItemStack stack = player.getStackInHand(hand);
-                if (stack.getItem() instanceof BlockItem) {
-                    BlockPos placementPos = hitResult.getBlockPos().offset(hitResult.getSide());
-                    player.sendMessage(Text.literal("[DEBUG] You placed a block: " + stack.getName().getString() + " at " + placementPos.toShortString()), false);
-                    NarratorTest.eventLogger.appendEvent("Place Block", stack.getName().getString(), System.currentTimeMillis());
+                // Check if the block is interactable (e.g., chest, furnace)
+                if (isInteractableBlock(interactedBlock)) {
+                    // Log the block interaction
+                    player.sendMessage(
+                        Text.literal("[DEBUG] You interacted with a block: " + interactedBlock.getName().getString() + " at " + interactedPos.toShortString()),
+                        false
+                    );
+                    NarratorTest.eventLogger.appendEvent("Interact Block", interactedBlock.getName().getString(), System.currentTimeMillis());
+                } else {
+                    // Check if the player is placing a block
+                    ItemStack stack = player.getStackInHand(hand);
+                    if (stack.getItem() instanceof BlockItem) {
+                        BlockPos placementPos = hitResult.getBlockPos().offset(hitResult.getSide());
+                        player.sendMessage(
+                            Text.literal("[DEBUG] You placed a block: " + stack.getName().getString() + " at " + placementPos.toShortString()),
+                            false
+                        );
+                        NarratorTest.eventLogger.appendEvent("Place Block", stack.getName().getString(), System.currentTimeMillis());
+                    }
                 }
             }
             return ActionResult.PASS; // Allow the interaction to proceed
         });
+    }
+
+    private static boolean isInteractableBlock(Block block) {
+    // List of interactable blocks
+    return block instanceof ChestBlock || // Chests
+           block instanceof FurnaceBlock || // Furnaces
+           block instanceof CraftingTableBlock || // Crafting tables
+           block instanceof AnvilBlock || // Anvils
+           block instanceof EnchantingTableBlock || // Enchanting tables
+           block instanceof EnderChestBlock || // Ender chests
+           block instanceof ShulkerBoxBlock || // Shulker boxes
+           block instanceof BarrelBlock || // Barrels
+           block instanceof DispenserBlock || // Dispensers
+           block instanceof DropperBlock || // Droppers
+           block instanceof HopperBlock || // Hoppers
+           block instanceof BrewingStandBlock || // Brewing stands
+           block instanceof BeaconBlock || // Beacons
+           block instanceof ComparatorBlock || // Redstone comparators
+           block instanceof RepeaterBlock || // Redstone repeaters
+           block instanceof LeverBlock || // Levers
+           block instanceof ButtonBlock || // Buttons
+           block instanceof DoorBlock || // Doors
+           block instanceof TrapdoorBlock || // Trapdoors
+           block instanceof FenceGateBlock || // Fence gates
+           block instanceof NoteBlock || // Note blocks
+           block instanceof JukeboxBlock || // Jukeboxes
+           block instanceof LecternBlock || // Lecterns
+           block instanceof LoomBlock || // Looms
+           block instanceof StonecutterBlock || // Stonecutters
+           block instanceof SmithingTableBlock || // Smithing tables
+           block instanceof CartographyTableBlock || // Cartography tables
+           block instanceof GrindstoneBlock || // Grindstones
+           block instanceof BellBlock || // Bells
+           block instanceof CampfireBlock || // Campfires
+           block instanceof RespawnAnchorBlock || // Respawn anchors
+           block instanceof CommandBlock || // Command blocks
+           block instanceof StructureBlock || // Structure blocks
+           block instanceof JigsawBlock || // Jigsaw blocks
+           block instanceof AbstractSignBlock || // Signs
+           block instanceof AbstractBannerBlock; // Banners
     }
 }
