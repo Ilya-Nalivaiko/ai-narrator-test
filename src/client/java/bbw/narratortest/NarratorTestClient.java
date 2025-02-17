234d31a97c61cc85adbe2b0e933e998499d19087
James <78901538+James-Laidlaw@users.noreply.github.com>
2025-02-16 20:16:14 -0700

    formatting

--- src/client/java/bbw/narratortest/NarratorTestClient.java
+++ src/client/java/bbw/narratortest/NarratorTestClient.java
@@ -168,9 +168,7 @@ public class NarratorTestClient implements ClientModInitializer {
           }
         });
 
-    for (
-
-        int i = 0; i < 4; i++) {
+    for (int i = 0; i < 4; i++) {
       armorState.add("");
     }
 
@@ -194,51 +192,60 @@ public class NarratorTestClient implements ClientModInitializer {
         BlockState interactedState = world.getBlockState(interactedPos);
         Block interactedBlock = interactedState.getBlock();
 
-                // Check if the block is interactable (e.g., chest, furnace)
-                if (isInteractableBlock(interactedBlock)) {
-                    if(System.currentTimeMillis() > nextAllowedBlockUse){
-                        // Log the block interaction
-                        NarratorTest.sendLogSuccessMessage("You interacted with a block: " + Registries.BLOCK.getEntry(interactedBlock).getIdAsString() + " at " + interactedPos.toShortString(), player);
-                        NarratorTest.addEvent(player, "Interact Block", Registries.BLOCK.getEntry(interactedBlock).getIdAsString(), System.currentTimeMillis());
-                        nextAllowedBlockUse = System.currentTimeMillis() + 150;
-                    } else NarratorTest.sendLogFailMessage("Remaining block interaction cooldown: " + Long.toString(nextAllowedBlockUse-System.currentTimeMillis()), player);
-                    } else {
-                    // Check if the player is placing a block
-                    ItemStack stack = player.getStackInHand(hand);
-                    if (stack.getItem() instanceof BlockItem) {
-                        if(System.currentTimeMillis() > nextAllowedBlockPlace){
-                            BlockPos placementPos = hitResult.getBlockPos().offset(hitResult.getSide());
-                            NarratorTest.sendLogSuccessMessage("You placed a block: " + stack.getRegistryEntry().getIdAsString() + " at " + placementPos.toShortString(), player);
-                            NarratorTest.addEvent(player, "Place Block", stack.getRegistryEntry().getIdAsString(), System.currentTimeMillis());
-                            nextAllowedBlockPlace = System.currentTimeMillis() + 190;
-                        } else NarratorTest.sendLogFailMessage("Remaining block place cooldown: " + Long.toString(nextAllowedBlockPlace-System.currentTimeMillis()), player);
-                        }
-                }
-            }
-            return ActionResult.PASS; // Allow the interaction to proceed
-        });
+        // Check if the block is interactable (e.g., chest, furnace)
+        if (isInteractableBlock(interactedBlock)) {
+          if (System.currentTimeMillis() > nextAllowedBlockUse) {
+            // Log the block interaction
+            NarratorTest.sendLogSuccessMessage("You interacted with a block: "
+                + Registries.BLOCK.getEntry(interactedBlock).getIdAsString() + " at " + interactedPos.toShortString(),
+                player);
+            NarratorTest.addEvent(player, "Interact Block", Registries.BLOCK.getEntry(interactedBlock).getIdAsString(),
+                System.currentTimeMillis());
+            nextAllowedBlockUse = System.currentTimeMillis() + 150;
+          } else
+            NarratorTest.sendLogFailMessage("Remaining block interaction cooldown: "
+                + Long.toString(nextAllowedBlockUse - System.currentTimeMillis()), player);
+        } else {
+          // Check if the player is placing a block
+          ItemStack stack = player.getStackInHand(hand);
+          if (stack.getItem() instanceof BlockItem) {
+            if (System.currentTimeMillis() > nextAllowedBlockPlace) {
+              BlockPos placementPos = hitResult.getBlockPos().offset(hitResult.getSide());
+              NarratorTest.sendLogSuccessMessage("You placed a block: " + stack.getRegistryEntry().getIdAsString()
+                  + " at " + placementPos.toShortString(), player);
+              NarratorTest.addEvent(player, "Place Block", stack.getRegistryEntry().getIdAsString(),
+                  System.currentTimeMillis());
+              nextAllowedBlockPlace = System.currentTimeMillis() + 190;
+            } else
+              NarratorTest.sendLogFailMessage("Remaining block place cooldown: "
+                  + Long.toString(nextAllowedBlockPlace - System.currentTimeMillis()), player);
+          }
+        }
+      }
+      return ActionResult.PASS; // Allow the interaction to proceed
+    });
 
+    // Equip armor event
+    ClientTickEvents.END_CLIENT_TICK.register(client -> {
+      PlayerEntity player = MinecraftClient.getInstance().player;
+      if (player != null) {
+        int i = 0;
+        for (ItemStack stack : player.getArmorItems()) {
+          if (!stack.isEmpty() && armorState.get(i).isEmpty()) {
+            armorState.set(i, stack.getRegistryEntry().getIdAsString());
+            NarratorTest.sendLogSuccessMessage("You equipped: " + stack.getRegistryEntry().getIdAsString(), player);
+            NarratorTest.addEvent(player, "Equip", stack.getRegistryEntry().getIdAsString(),
+                System.currentTimeMillis());
 
-        // Equip armor event
-        ClientTickEvents.END_CLIENT_TICK.register(client -> {
-            PlayerEntity player = MinecraftClient.getInstance().player;
-            if (player != null) {
-                int i = 0;
-                for (ItemStack stack : player.getArmorItems()) {
-                    if (!stack.isEmpty() && armorState.get(i).isEmpty()) {
-                        armorState.set(i, stack.getRegistryEntry().getIdAsString());
-                        NarratorTest.sendLogSuccessMessage("You equipped: " + stack.getRegistryEntry().getIdAsString(), player);
-                        NarratorTest.addEvent(player, "Equip", stack.getRegistryEntry().getIdAsString(), System.currentTimeMillis());
-                        
-                    } else if (stack.isEmpty() && !armorState.get(i).isEmpty()){
-                        NarratorTest.sendLogSuccessMessage("You unequipped: " + armorState.get(i), player);
-                        NarratorTest.addEvent(player, "Unequip", armorState.get(i), System.currentTimeMillis());
-                        armorState.set(i, "");
-                    }
-                    i++;
-                }
-            }
-        });
+          } else if (stack.isEmpty() && !armorState.get(i).isEmpty()) {
+            NarratorTest.sendLogSuccessMessage("You unequipped: " + armorState.get(i), player);
+            NarratorTest.addEvent(player, "Unequip", armorState.get(i), System.currentTimeMillis());
+            armorState.set(i, "");
+          }
+          i++;
+        }
+      }
+    });
 
     // Breeding and taming animals, and trading with villagers event
     UseEntityCallback.EVENT.register((player, world, hand, entity, hitResult) -> {
@@ -246,28 +253,38 @@ public class NarratorTestClient implements ClientModInitializer {
         AnimalEntity animal = (AnimalEntity) entity;
         ItemStack heldItem = player.getStackInHand(hand);
 
-                if (animal.isBreedingItem(heldItem)) {
-                    if(System.currentTimeMillis() > nextAllowedBreed){
-                        NarratorTest.sendLogSuccessMessage("You bred: " + Registries.ENTITY_TYPE.getEntry(entity.getType()).getIdAsString() + " using " + heldItem.getName().getString(), player);
-                        NarratorTest.addEvent(player, "Bred", Registries.ENTITY_TYPE.getEntry(entity.getType()).getIdAsString() + " using " + heldItem.getName().getString(), System.currentTimeMillis());
-                        nextAllowedBreed = System.currentTimeMillis() + 300;
-                    } else NarratorTest.sendLogFailMessage("Remaining breed cooldown: " + Long.toString(nextAllowedBreed-System.currentTimeMillis()), player);
-                    
-                } else {
-                    NarratorTest.sendLogFailMessage("You tried to breed: " + Registries.ENTITY_TYPE.getEntry(entity.getType()).getIdAsString() + " with an invalid item", player);
-                }
-            }
-            if (entity instanceof net.minecraft.entity.passive.VillagerEntity) {
-                if(System.currentTimeMillis() > nextAllowedTrade){
-                    NarratorTest.sendLogSuccessMessage("You traded with : " + Registries.ENTITY_TYPE.getEntry(entity.getType()).getIdAsString(), player);
-                    NarratorTest.addEvent(player, "Traded with", Registries.ENTITY_TYPE.getEntry(entity.getType()).getIdAsString(), System.currentTimeMillis());
-                    nextAllowedTrade = System.currentTimeMillis() + 500;
-                } else NarratorTest.sendLogFailMessage("Remaining trade cooldown: " + Long.toString(nextAllowedTrade-System.currentTimeMillis()), player);
-                
-            }
-            return ActionResult.PASS;
-        });
-    }
+        if (animal.isBreedingItem(heldItem)) {
+          if (System.currentTimeMillis() > nextAllowedBreed) {
+            NarratorTest
+                .sendLogSuccessMessage("You bred: " + Registries.ENTITY_TYPE.getEntry(entity.getType()).getIdAsString()
+                    + " using " + heldItem.getName().getString(), player);
+            NarratorTest.addEvent(player, "Bred", Registries.ENTITY_TYPE.getEntry(entity.getType()).getIdAsString()
+                + " using " + heldItem.getName().getString(), System.currentTimeMillis());
+            nextAllowedBreed = System.currentTimeMillis() + 300;
+          } else
+            NarratorTest.sendLogFailMessage(
+                "Remaining breed cooldown: " + Long.toString(nextAllowedBreed - System.currentTimeMillis()), player);
+
+        } else {
+          NarratorTest.sendLogFailMessage("You tried to breed: "
+              + Registries.ENTITY_TYPE.getEntry(entity.getType()).getIdAsString() + " with an invalid item", player);
+        }
+      }
+      if (entity instanceof net.minecraft.entity.passive.VillagerEntity) {
+        if (System.currentTimeMillis() > nextAllowedTrade) {
+          NarratorTest.sendLogSuccessMessage(
+              "You traded with : " + Registries.ENTITY_TYPE.getEntry(entity.getType()).getIdAsString(), player);
+          NarratorTest.addEvent(player, "Traded with",
+              Registries.ENTITY_TYPE.getEntry(entity.getType()).getIdAsString(), System.currentTimeMillis());
+          nextAllowedTrade = System.currentTimeMillis() + 500;
+        } else
+          NarratorTest.sendLogFailMessage(
+              "Remaining trade cooldown: " + Long.toString(nextAllowedTrade - System.currentTimeMillis()), player);
+
+      }
+      return ActionResult.PASS;
+    });
+  }
 
   private static boolean isInteractableBlock(Block block) {
     // List of interactable blocks
